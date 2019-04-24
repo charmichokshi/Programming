fe7979e9b6fda0580700ccfdbdc147e1334a0013
Charmi Chokshi <charmichokshi@gmail.com>
2019-04-25 00:32:22 +0530

    Add files via upload

---
+++ Cracking The Coding Interview Problems/longest_word_in_dict_from_string.py
@@ -0,0 +1,21 @@
+def find_longest_word(s, d):
+	ele = str()
+	max_len = 0
+
+	for word in d:
+		yes, start = 0, 0 
+		l = len(s)
+		for char in word:
+			for c in range(start, l):
+				if s[c] == char:
+					yes += 1
+					start = s.index(s[c])+1
+					break
+		if yes == len(word):
+			if len(word) >= max_len:
+				max_len = len(word)
+				ele = word
+	return ele
+
+longest_word = find_longest_word("abppplee", {"a", "able", "ale", "apple", "bale", "kangaroo"})
+print(longest_word)
